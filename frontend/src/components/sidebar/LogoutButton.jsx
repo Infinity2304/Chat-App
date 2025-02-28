fce8ce71439b24e4820ac336b05a0c0250db2eb4
Satish Narkar <Infinity2304>
2025-02-28 19:36:54 +0530

    frontend part done

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TbLogout2 } from "react-icons/tb";
+
+const LogoutButton = () => {
+    return <div className='my-auto pt-2'>
+        <TbLogout2 className='w-6 h-6 text-white cursor-pointer'/>
+    </div>
+
+}
+
+export default LogoutButton
